d0eeb1acdc8eff67d71fb87fb1758cfece938948
Braulio Diez Botella <braulio.diez@gmail.com>
2023-09-18 11:49:41 +0200

    boilerplate

---
+++ 00-boilerplate/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>Redux 2023 - Boilerplate</header>
+      <main>Aquí van las demos..</main>
+    </>
+  );
+}
+
+export default App;
